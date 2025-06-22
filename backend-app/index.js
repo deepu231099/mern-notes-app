24b2922e6323bb12d4f2aed5d9a48ab7f376cb89
deepak <deepak.gupta@salesfine.co>
2025-06-22 10:34:34 +0530

    server-file-added

---
+++ backend-app/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import noteRoutes from "./routes/notes.js"
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/notes", noteRoutes);
+
+const PORT = 5000;
+
+
+mongoose
+    .connect("mongodb://mongo:27017/notesdb")
+    .then(() => {
+        app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+    })
+    .catch((err) => console.error("❌ MongoDB connection error:", err));
